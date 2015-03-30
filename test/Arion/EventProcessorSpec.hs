cbae57074e9a9cf0842cadcb26539ead2c27aa22
Karun <karun012@gmail.com>
2015-03-29 21:30:42 -0700

    I'm a Java guy. Trying to inline things and make it less verbose

--- test/Arion/EventProcessorSpec.hs
+++ test/Arion/EventProcessorSpec.hs
@@ -19,75 +19,56 @@ spec = do
             let sourceFilePathA = "src/ModuleA.hs"
             let testFileA = TestFile "test/ModuleASpec.hs" ["ModuleA"]
             let testFileB = TestFile "test/ModuleBSpec.hs" ["ModuleB"]
-            let testFolder = "test"
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let modifiedEvent = Modified "mydir/ModuleASpec.hs" sampleTime
-            let sourceFolder = "src"
-            let expectedCommands = [Echo "mydir/ModuleASpec.hs changed", CabalExec $ RunHaskell "src" "test" "mydir/ModuleASpec.hs"]
 
-            processEvent sourceToTestFileMap sourceFolder testFolder modifiedEvent `shouldBe` expectedCommands
+            processEvent sourceToTestFileMap "src" "test" modifiedEvent `shouldBe` [Echo "mydir/ModuleASpec.hs changed", CabalExec $ RunHaskell "src" "test" "mydir/ModuleASpec.hs"]
         it "responds to a Modified event on a source file by creating commands to run the associated tests" $ do
             let sourceFilePathA = "src/ModuleA.hs"
             let testFileA = TestFile "test/ModuleASpec.hs" ["ModuleA"]
             let testFileB = TestFile "test/ModuleBSpec.hs" ["ModuleB"]
-            let testFolder = "test"
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let modifiedEvent = Modified "src/ModuleA.hs" sampleTime
-            let sourceFolder = "src"
-            let expectedCommands = [Echo "src/ModuleA.hs changed", CabalExec $ RunHaskell "src" "test" "test/ModuleASpec.hs",
-                                       CabalExec $ RunHaskell "src" "test" "test/ModuleBSpec.hs"]
 
-            processEvent sourceToTestFileMap sourceFolder testFolder modifiedEvent `shouldBe` expectedCommands
+            processEvent sourceToTestFileMap "src" "test" modifiedEvent `shouldBe` [Echo "src/ModuleA.hs changed", CabalExec $ RunHaskell "src" "test" "test/ModuleASpec.hs",
+                                                                                       CabalExec $ RunHaskell "src" "test" "test/ModuleBSpec.hs"]
         it "responds to a Added event on a test file by creating commands to run tests" $ do
             let sourceFilePathA = "src/ModuleA.hs"
             let testFileA = TestFile "test/ModuleASpec.hs" ["ModuleA"]
             let testFileB = TestFile "test/ModuleBSpec.hs" ["ModuleB"]
-            let testFolder = "test"
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let addedEvent = Added "mydir/ModuleASpec.hs" sampleTime
-            let sourceFolder = "src"
-            let expectedCommands = [Echo "mydir/ModuleASpec.hs changed", CabalExec $ RunHaskell "src" "test" "mydir/ModuleASpec.hs"]
 
-            processEvent sourceToTestFileMap sourceFolder testFolder addedEvent `shouldBe` expectedCommands
+            processEvent sourceToTestFileMap "src" "test" addedEvent `shouldBe` [Echo "mydir/ModuleASpec.hs changed", CabalExec $ RunHaskell "src" "test" "mydir/ModuleASpec.hs"]
         it "responds to a Added event on a source file by creating commands to run the associated tests" $ do
             let sourceFilePathA = "src/ModuleA.hs"
             let testFileA = TestFile "test/ModuleASpec.hs" ["ModuleA"]
             let testFileB = TestFile "test/ModuleBSpec.hs" ["ModuleB"]
-            let testFolder = "test"
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let addedEvent = Added "src/ModuleA.hs" sampleTime
-            let sourceFolder = "src"
-            let expectedCommands = [Echo "src/ModuleA.hs changed", CabalExec $ RunHaskell "src" "test" "test/ModuleASpec.hs",
-                                       CabalExec $ RunHaskell "src" "test" "test/ModuleBSpec.hs"]
 
-
-            processEvent sourceToTestFileMap sourceFolder testFolder addedEvent `shouldBe` expectedCommands
+            processEvent sourceToTestFileMap "src" "test" addedEvent `shouldBe` [Echo "src/ModuleA.hs changed", CabalExec $ RunHaskell "src" "test" "test/ModuleASpec.hs",
         it "ignores non haskell source files" $ do
             let sourceFilePathA = "src/ModuleA.hs"
             let testFileA = TestFile "test/ModuleASpec.hs" ["ModuleA"]
             let testFileB = TestFile "test/ModuleBSpec.hs" ["ModuleB"]
-            let testFolder = "test"
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let modifiedEvent = Modified "mydir/ModuleASpec.hs~" sampleTime
-            let expectedCommands = []
-            let sourceFolder = "src"
 
-            processEvent sourceToTestFileMap sourceFolder testFolder modifiedEvent `shouldBe` expectedCommands
+            processEvent sourceToTestFileMap "src" "test" modifiedEvent `shouldBe` []
 
             let addedEvent = Added "mydir/ModuleASpec.swp" sampleTime
 
-            processEvent sourceToTestFileMap sourceFolder testFolder addedEvent `shouldBe` expectedCommands
+            processEvent sourceToTestFileMap "src" "test" addedEvent `shouldBe` []
         it "does not ignore lhs files" $ do
             let sourceFilePathA = "src/ModuleA.lhs"
             let testFileA = TestFile "test/ModuleASpec.lhs" ["ModuleA"]
             let testFileB = TestFile "test/ModuleBSpec.lhs" ["ModuleB"]
-            let testFolder = "test"
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let addedEvent = Added "mydir/ModuleASpec.lhs" sampleTime
-            let sourceFolder = "src"
-            let expectedCommands = [Echo "mydir/ModuleASpec.lhs changed", CabalExec $ RunHaskell "src" "test" "mydir/ModuleASpec.lhs"]
 
-            processEvent sourceToTestFileMap sourceFolder testFolder addedEvent `shouldBe` expectedCommands
+            processEvent sourceToTestFileMap "src" "test" addedEvent `shouldBe` [Echo "mydir/ModuleASpec.lhs changed", CabalExec $ RunHaskell "src" "test" "mydir/ModuleASpec.lhs"]
+
 sampleTime :: UTCTime
 sampleTime = UTCTime (ModifiedJulianDay 2) (secondsToDiffTime 2)
 
