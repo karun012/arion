acac993203c95ba83d32fe9916aece0ea8b2ee86
Karun <karun012@gmail.com>
2015-04-02 18:04:54 -0700

    Revert "#23 - Added better echo commands when a source file changes" - we will get back to this once zen-mode is implemented
    This reverts commit 86f019b64e1d225af087930cb5e2f40465d7b192.

--- test/Arion/EventProcessorSpec.hs
+++ test/Arion/EventProcessorSpec.hs
@@ -23,9 +23,8 @@ spec = do
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let modifiedEvent = fromJust . respondToEvent $ Modified "mydir/ModuleASpec.hs" sampleTime
 
-            processEvent sourceToTestFileMap "src" "test" [] modifiedEvent `shouldBe` [Echo "mydir/ModuleASpec.hs changed", RunHaskell "src" "test" "mydir/ModuleASpec.hs"]
+            processEvent sourceToTestFileMap "src" "test" modifiedEvent `shouldBe` [Echo "mydir/ModuleASpec.hs changed", RunHaskell "src" "test" "mydir/ModuleASpec.hs"]
         it "responds to a Modified event on a source file by creating commands to run the associated tests" $ do
-            let sourceFile = SourceFile { sourceFilePath = "src/ModuleA.hs", moduleName = "ModuleA", importedModules = ["Module1", "Another.Module", "Yet.Another.Module"] }
             let sourceFilePathA = "src/ModuleA.hs"
             let testFileA = TestFile "test/ModuleASpec.hs" ["ModuleA"]
             let testFileB = TestFile "test/ModuleBSpec.hs" ["ModuleB"]
@@ -33,12 +32,8 @@ spec = do
             let modifiedEvent = fromJust . respondToEvent $
                                 Modified "src/ModuleA.hs" sampleTime
 
-            processEvent sourceToTestFileMap "src" "test" [sourceFile] modifiedEvent `shouldBe` [Echo "ModuleA changed",
-                                                                                       Echo "ModuleA is associated with these tests",
-                                                                                       Echo "test/ModuleASpec.hs",
-                                                                                       Echo "test/ModuleBSpec.hs",
-                                                                                       RunHaskell "src" "test" "test/ModuleASpec.hs",
-                                                                                       RunHaskell "src" "test" "test/ModuleBSpec.hs"]
+            processEvent sourceToTestFileMap "src" "test" modifiedEvent `shouldBe` [Echo "src/ModuleA.hs changed", RunHaskell "src" "test" "test/ModuleASpec.hs",
+                                                                                    RunHaskell "src" "test" "test/ModuleBSpec.hs"]
         it "responds to a Added event on a test file by creating commands to run tests" $ do
             let sourceFilePathA = "src/ModuleA.hs"
             let testFileA = TestFile "test/ModuleASpec.hs" ["ModuleA"]
@@ -46,21 +41,16 @@ spec = do
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let addedEvent = fromJust . respondToEvent $ Added "mydir/ModuleASpec.hs" sampleTime
 
-            processEvent sourceToTestFileMap "src" "test" [] addedEvent `shouldBe` [Echo "mydir/ModuleASpec.hs changed", RunHaskell "src" "test" "mydir/ModuleASpec.hs"]
+            processEvent sourceToTestFileMap "src" "test" addedEvent `shouldBe` [Echo "mydir/ModuleASpec.hs changed", RunHaskell "src" "test" "mydir/ModuleASpec.hs"]
         it "responds to a Added event on a source file by creating commands to run the associated tests" $ do
-            let sourceFile = SourceFile { sourceFilePath = "src/ModuleA.hs", moduleName = "ModuleA", importedModules = ["Module1", "Another.Module", "Yet.Another.Module"] }
             let sourceFilePathA = "src/ModuleA.hs"
             let testFileA = TestFile "test/ModuleASpec.hs" ["ModuleA"]
             let testFileB = TestFile "test/ModuleBSpec.hs" ["ModuleB"]
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let addedEvent = fromJust . respondToEvent $ Added "src/ModuleA.hs" sampleTime
 
-            processEvent sourceToTestFileMap "src" "test" [sourceFile] addedEvent `shouldBe` [Echo "ModuleA changed",
-                                                                                              Echo "ModuleA is associated with these tests",
-                                                                                              Echo "test/ModuleASpec.hs",
-                                                                                              Echo "test/ModuleBSpec.hs",
-                                                                                              RunHaskell "src" "test" "test/ModuleASpec.hs",
-                                                                                              RunHaskell "src" "test" "test/ModuleBSpec.hs"]
+            processEvent sourceToTestFileMap "src" "test" addedEvent `shouldBe` [Echo "src/ModuleA.hs changed", RunHaskell "src" "test" "test/ModuleASpec.hs",
+                                                                                        RunHaskell "src" "test" "test/ModuleBSpec.hs"]
         it "ignores non haskell source files" $ do
             let sourceFilePathA = "src/ModuleA.hs"
             let testFileA = TestFile "test/ModuleASpec.hs" ["ModuleA"]
@@ -68,11 +58,11 @@ spec = do
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let modifiedEvent = fromJust . respondToEvent $ Modified "mydir/ModuleASpec.hs~" sampleTime
 
-            processEvent sourceToTestFileMap "src" "test" [] modifiedEvent `shouldBe` []
+            processEvent sourceToTestFileMap "src" "test" modifiedEvent `shouldBe` []
 
             let addedEvent = fromJust . respondToEvent $ Added "mydir/ModuleASpec.swp" sampleTime
 
-            processEvent sourceToTestFileMap "src" "test" [] addedEvent `shouldBe` []
+            processEvent sourceToTestFileMap "src" "test" addedEvent `shouldBe` []
         it "does not ignore lhs files" $ do
             let sourceFilePathA = "src/ModuleA.lhs"
             let testFileA = TestFile "test/ModuleASpec.lhs" ["ModuleA"]
@@ -80,13 +70,13 @@ spec = do
             let sourceToTestFileMap = fromList [(sourceFilePathA, [testFileA, testFileB])]
             let addedEvent = fromJust . respondToEvent $ Added "mydir/ModuleASpec.lhs" sampleTime
 
-            processEvent sourceToTestFileMap "src" "test" [] addedEvent `shouldBe` [Echo "mydir/ModuleASpec.lhs changed", RunHaskell "src" "test" "mydir/ModuleASpec.lhs"]
+            processEvent sourceToTestFileMap "src" "test" addedEvent `shouldBe` [Echo "mydir/ModuleASpec.lhs changed", RunHaskell "src" "test" "mydir/ModuleASpec.lhs"]
         it "tells you when a source file does not have any tests" $ do
             let sourceFilePathA = "src/ModuleA.hs"
             let sourceToTestFileMap = fromList [(sourceFilePathA, [])]
             let addedEvent = fromJust . respondToEvent $ Added "src/ModuleA.hs" sampleTime
 
-            processEvent sourceToTestFileMap "src" "test" [] addedEvent `shouldBe` [Echo "src/ModuleA.hs changed", Echo "src/ModuleA.hs does not have any associated tests..."]
+            processEvent sourceToTestFileMap "src" "test" addedEvent `shouldBe` [Echo "src/ModuleA.hs changed", Echo "src/ModuleA.hs does not have any associated tests..."]
 
 sampleTime :: UTCTime
 sampleTime = UTCTime (ModifiedJulianDay 2) (secondsToDiffTime 2)
