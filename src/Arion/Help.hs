b183eb4fe62d0202313cb236656916df8e5b85fb
Simon Hengel <sol@typeful.net>
2015-04-05 12:43:05 +0800

    Update help message

--- src/Arion/Help.hs
+++ src/Arion/Help.hs
@@ -4,5 +4,7 @@ module Arion.Help (
 
 usage :: String
 usage = unlines $ ["Usage: arion <folder to watch> <folder with source files> <folder with test files>",
+                   "       arion",
+                   "\nIf invoked without argumets '.', 'src' and 'test' are assumed for \"folder to watch\", \"folder with source files\" and \"folder with test files\" respectively.",
                    "\n  --version",
                    "\tshows version information"]
