c38941d77a7db43793e2768d8bc7f632c5c2fcf6
Karun <karun012@gmail.com>
2015-04-05 20:04:15 -0700

    Changing help text

--- src/Arion/Help.hs
+++ src/Arion/Help.hs
@@ -3,7 +3,7 @@ module Arion.Help (
 ) where
 
 usage :: String
-usage = unlines ["arion by default assumes that it needs to watch the current folder for changes, and that src and test are the source and test folders.",
-                "\nUsage: arion <folder to watch> <folder with source files> <folder with test files>", 
+usage = unlines ["Usage: arion [folder to watch] [folder with source files] [folder with test files]",
                 "\n  --version",
                 "\tshows version information"]
+
--- test/Arion/HelpSpec.hs
+++ test/Arion/HelpSpec.hs
@@ -9,7 +9,6 @@ main = hspec spec
 spec = do
     describe "Help" $ do
         it "can show how to use arion" $ do
-            usage `shouldBe` unlines ["arion by default assumes that it needs to watch the current folder for changes, and that src and test are the source and test folders.",
-                                      "\nUsage: arion <folder to watch> <folder with source files> <folder with test files>", 
+            usage `shouldBe` unlines ["Usage: arion [folder to watch] [folder with source files] [folder with test files]", 
                                       "\n  --version",
                                       "\tshows version information"]
